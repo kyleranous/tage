495d4d1677348ce2506cd1f0a49ad41f606d2e1e
Kyle Ranous <kranous@Concept32designs.com>
2021-12-01 21:55:17 -0700

    Started basic map with movement around the room

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 from colorama import Fore, Style
 
 
@@ -9,7 +10,8 @@ noList = ['NAH', 'NO', 'N', 'Nah', 'No', 'n', 'nah', 'no']
 
 def drawTitleScreen(errorMSG=None): # Draw the Title Screen
     clearConsole()
-    print(Fore.GREEN + Style.BRIGHT + "     /########  /##        /########   /######   /##   /##   /######   /#######   /##  /######")
+    print(Fore.GREEN + Style.BRIGHT)
+    print("     /########  /##        /########   /######   /##   /##   /######   /#######   /##  /######")
     print("    | ##_____/ | ##       | ##_____/  /##__  ## | ### | ##  /##__  ## | ##__  ## | #/ /##__  ##")
     print("    | ##       | ##       | ##       | ##  \ ## | ####| ## | ##  \ ## | ##  \ ## |_/ | ##  \__/")
     print("    | #####    | ##       | #####    | ######## | ## ## ## | ##  | ## | #######/     |  ######")
@@ -27,7 +29,8 @@ def drawTitleScreen(errorMSG=None): # Draw the Title Screen
     print("| ##  | ## | #######/    \  #/    | ######## | ## \  ##    | ##    |  ######/ | ##  | ## | ########")
     print("|__/  |__/ |_______/      \_/     |________/ |__/  \__/    |__/     \______/  |__/  |__/ |________/")
     print(Fore.RESET + Style.NORMAL + "\nVersion: 0.1a")
-    print(Fore.BLUE + Style.BRIGHT + "***************************************************************************************************")
+    print(Fore.BLUE + Style.BRIGHT)
+    print("***************************************************************************************************")
     print("*                                             SOMETHING                                           *")
     print("*                                           INSPERATIONAL                                         *")
     print("***************************************************************************************************")
@@ -43,10 +46,136 @@ def drawTitleScreen(errorMSG=None): # Draw the Title Screen
     if ans in yesList:
         clearConsole()
         print("Every day we write a new page to our story.")
+        time.sleep(3)
+        # Call the room function
+        mapIntro()
+        
     elif ans in noList:
         clearConsole()
         print("Tommorow I'll be all the things I tried to be today.")
     else:
         drawTitleScreen("I'm sorry, I do not understand {}.".format(ans))
 
-drawTitleScreen()
+
+# Room Layout
+#      _________________________
+#     |            |            |
+#     | bookShelf  |     bed    |
+#     |            |            |
+#     |            |            |
+#     |------------+------------|        N
+#     |            |            |        |
+#     |   closet   |   window   |    W---+---E
+#     |            |            |        |
+#     |            |            |        S
+#     |------------+------------| 
+#     |            |            |
+#     |  entryWay  |    tipi    |
+#     |            |            |
+#     |____________|____________|         
+
+def mapIntro():
+    
+    clearConsole()
+    print( """\tINTRODUCTION TEXT TO ROOM
+    """)
+    print('\n')
+    entryWay()
+
+
+def entryWay(errorMsg=None):
+    
+    print("\tYou are in the entry way.\n")
+    if errorMsg is not None:
+        print(Fore.RED + errorMsg)
+        print(Fore.RESET)
+
+    print(Style.BRIGHT)
+    act = input('> ')
+    print(Style.RESET_ALL)
+    
+    # North -> closet
+    # East -> tipi
+    # South -> Wall
+    # West -> exitGame
+
+    if act == "go north":
+        closet()
+    elif act == "go east":
+        tipi()
+    elif act == "go south":
+        #clearConsole()
+        entryWay(errorMsg="There is a wall")
+    elif act == "go west":
+        exitGame()
+    else:
+        entryWay(errorMsg = "I'm sorry, I do not understand {}.".format(act))
+
+
+def closet(errorMsg = None):
+    
+    print("\tYou are standing infront of the closet.\n")
+    if errorMsg is not None:
+        print(Fore.RED + errorMsg)
+        print(Fore.RESET)
+    
+    print(Style.BRIGHT)
+    act = input('> ')
+    print(Style.RESET_ALL + '\n')
+
+    if act == "go north":
+        bookShelf()
+    elif act == "go east":
+        window()
+    elif act == "go south":
+        #clearConsole()
+        entryWay()
+    elif act == "go west":
+        closet(errorMsg = "You can not go into the closet")
+    else:
+        entryWay(errorMsg = "I'm sorry, I do not understand {}.".format(act))
+    # North -> bookShelf
+    # East -> window
+    # South -> entryWay
+    # West -> Wall
+
+
+def bookShelf():
+    print("\tYou are standing infront of a book shelf")
+
+
+def bed():
+    print("\tYou are standing infront of a bed")
+
+
+def window():
+    print("You are standing infront of a window")
+
+
+def tipi():
+    print("You are standing infront of a tipi")
+
+
+def exitGame(errorMsg = None):
+    
+    if errorMsg is not None:
+        print(Fore.RED + errorMsg)
+        print(Fore.RESET)
+
+    print("\tThis is a door leading out of the room. Would you like to leave?\n")
+
+    print(Style.BRIGHT)
+    ans = input('> ')
+    print(Style.RESET_ALL)
+
+    if ans in yesList:
+        clearConsole()
+        print("Tomorrow I'll be all the things I tried to be today")
+    elif ans in noList:
+        entryWay()
+    else:
+        exitGame(errorMsg = "I'm sorry, I do not understand {}.".format(ans))
+
+
+if __name__ == '__main__':
+    drawTitleScreen()

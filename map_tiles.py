92c5f2c62e4169a1bac83fc6b725643d8561daa5
Kyle Ranous <kranous@Concept32designs.com>
2021-12-03 13:36:42 -0700

    Added item management to MapTile Function

--- map_tiles.py
+++ map_tiles.py
@@ -5,7 +5,14 @@ class MapTile():
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.items = []
 
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self, item):
+        self.items.remove(item)
+        
     def __str__(self):
         return "Tile: {}\nDescription:{}".format(self.name, self.description)
 

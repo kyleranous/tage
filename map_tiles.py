a7be30f62a68a91db1e646ded7685f81fd08ccae
Kyle Ranous <kranous@Concept32designs.com>
2021-12-03 14:32:16 -0700

    Fixed formatting

--- map_tiles.py
+++ map_tiles.py
@@ -6,13 +6,15 @@ class MapTile():
         self.name = name
         self.description = description
         self.items = []
+        self.newTile = True
 
     def add_item(self, item):
         self.items.append(item)
 
     def remove_item(self, item):
         self.items.remove(item)
-        
+
+
     def __str__(self):
         return "Tile: {}\nDescription:{}".format(self.name, self.description)
 

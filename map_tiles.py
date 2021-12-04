cbf74ac41f4c1b0e74fb3b55d8fd659ff56c7df7
Kyle Ranous <kranous@Concept32designs.com>
2021-12-04 08:19:27 -0700

    Adjusted MapTile Super Class, added notes for MapTile subclasses

--- map_tiles.py
+++ map_tiles.py
@@ -2,19 +2,19 @@
 
 class MapTile():
     
-    def __init__(self, name, shortDescription):
+    def __init__(self, name, x, y):
         self.name = name
-        self.shortDescription = shortDescription
-        self.longDescription = shortDescription
-        self.items = []
-        self.newTile = True
+        self.x = x
+        self.y = y
 
-    def add_item(self, item):
-        self.items.append(item)
-
-    def remove_item(self, item):
-        self.items.remove(item)
+    def intro_text(self):
+        raise NotImplementedError("Create a subclass of MapTile")
 
     def __str__(self):
         return "Tile: {}\nDescription:{}".format(self.name, self.shortDescription)
 
+
+# Need to have subclasses of Map tiles
+# - StartTile - will be the starting tile on a map
+# - TransportTile - Will be a linking tile between 2 maps. 
+# - GameTile - Will be a typical game play tiles

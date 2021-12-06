280235597bf6ca82ae0496560e989a8de102985a
Kyle Ranous <kranous@Concept32designs.com>
2021-12-06 12:41:02 -0700

    Added money variable to class Player

--- tbage_player.py
+++ tbage_player.py
@@ -4,14 +4,24 @@ class Player():
     def __init__(self, name):
         self.name = name
         self.inventory = {}
+        self.money = 0
+        self.posX = 0
+        self.posY = 0
 
     def __str__(self):
         return f"Player: {self.name}"
 
     def adjust_inventory(self, itemKey, qty):
-        
+        ''' Adjusts players inventory, adds item if qty is positive and item does
+            not currently exists in inventroy, adjusts quantity if item already exists
+        '''
         if itemKey in self.inventory.keys():
+            # Check if inventory adjustment will lead to a negative number
             self.inventory[itemKey] = self.inventory[itemKey] + qty
+            
+            if self.inventory[itemKey] <= 0: # Remove item if quantity is less then or equal to 0
+                del(self.inventory[itemKey])
+            
             return 1
         
         else:
@@ -20,3 +30,4 @@ class Player():
             else:
                 self.inventory[itemKey] = qty
                 return 1
+

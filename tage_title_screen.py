362612b31b116f023fa9af87c195a39e4854a4e3
Kyle Ranous <kranous@Concept32designs.com>
2021-12-07 08:58:54 -0700

    Changed Title to a list to allow for multiple lines. Will add abilitiy to format these lines seperately

--- tage_title_screen.py
+++ tage_title_screen.py
@@ -5,7 +5,7 @@ from colorama import Fore
 class TitleScreen():
 
     def __init__(self, title):
-        self.title = title
+        self.title = [title]
         self.titleFont = "big"
         self.width = 122
         self.color = None
@@ -13,37 +13,41 @@ class TitleScreen():
         self.bannerColor = None
         self.caption = None
     
+    def add_title_line(self, title):
+        self.title.append(title)
+
     def add_banner_line(self, bannerText):
         self.bannerText.append(bannerText)
 
     def draw_title_screen(self):
-        if self.title is not None:
-            t = pyfiglet.figlet_format(self.title, font=self.titleFont, width=self.width, justify="center")
-            # Set title Color if there is one
-            if self.color is not None:
-                if self.color.lower() == "blue":
-                    print(Fore.BLUE)
-                elif self.color.lower() == "green":
-                    print(Fore.GREEN)
-                elif self.color.lower() == "red":
-                    print(Fore.RED)
-                elif self.color.lower() == "yellow":
-                    print(Fore.YELLOW)
-                elif self.color.lower() == "black":
-                    print(Fore.BLACK)
-                elif self.color.lower() == "cyan":
-                    print(Fore.CYAN)
-                elif self.color.lower() == "magenta":
-                    print(Fore.MAGENTA)
-                else:
-                    raise ValueError(f"{self.color} is not a valid color selection")
+        if len(self.title) > 0:
+            for t in self.title:
+                t = pyfiglet.figlet_format(t, font=self.titleFont, width=self.width, justify="center")
+                # Set title Color if there is one
+                if self.color is not None:
+                    if self.color.lower() == "blue":
+                        print(Fore.BLUE)
+                    elif self.color.lower() == "green":
+                        print(Fore.GREEN)
+                    elif self.color.lower() == "red":
+                        print(Fore.RED)
+                    elif self.color.lower() == "yellow":
+                        print(Fore.YELLOW)
+                    elif self.color.lower() == "black":
+                        print(Fore.BLACK)
+                    elif self.color.lower() == "cyan":
+                        print(Fore.CYAN)
+                    elif self.color.lower() == "magenta":
+                        print(Fore.MAGENTA)
+                    else:
+                        raise ValueError(f"{self.color} is not a valid color selection")
             
-            # Reset Title Color Selection
-            print(t + Fore.RESET + "\033[F")
+                # Reset Title Color Selection
+                print(t + Fore.RESET + "\033[F\033[F")
 
             # Print Caption if there is one
             if self.caption is not None:
-                print("\033[F" + self.caption)
+                print(self.caption)
 
             # Set Banner Color if there is one
             if self.bannerColor is not None:
@@ -82,14 +86,15 @@ class TitleScreen():
 
 def main():
 
-    testScreen = TitleScreen(title="E L E A N O R ' S\nA D V E N T U R E")
-    testScreen.color = "yellow"
+    testScreen = TitleScreen(title="E L E A N O R ' S")
+    testScreen.add_title_line("A D V E N T U R E")
+    testScreen.color = "green"
     testScreen.caption = "Version 0.1a"
     testScreen.width = 100
     #testScreen.titleFont = "doom"
     testScreen.add_banner_line("TEST")
     testScreen.add_banner_line("Test 2")
-    testScreen.bannerColor = "red"
+    testScreen.bannerColor = "blue"
     testScreen.draw_title_screen()
 
 

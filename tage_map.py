2e9c7f15fd03e093530df14e5e8f452206e43e5e
Kyle Ranous <kranous@Concept32designs.com>
2021-12-11 22:06:31 -0700

    Added add_item(), remove_item(), check_item(), has_inventory(), and tile_items() functions to class MapTile

--- tage_map.py
+++ tage_map.py
@@ -16,7 +16,7 @@ class MapTile():
         self.shortDescription = ""
         self.intro = ""
         self.tile_inspect = {}
-        self.map_items = []
+        self.map_items = {}
 
     def intro_text(self):
         raise NotImplementedError("Create a subclass of MapTile")
@@ -29,6 +29,55 @@ class MapTile():
             return True
         else:
             return False
+    
+    def add_item(self, item, qty):
+        if qty < 1:
+            raise ValueError("Please use subtract_item() to remove item from MapTile")
+        try:
+            item.is_item() # Function should fail at this check
+            if item.name.lower() in self.map_items:
+                # If the item is already in the map tile
+                # Increase the Value of the item's qty by qty
+                self.map_items[item.name.lower()]["qty"] += qty
+            else:
+                self.map_items[item.name.lower()] = {
+                    "item" : item,
+                    "qty" : qty
+                }
+
+        except:
+            raise ValueError("Item must be a tage Item() class or one of it's children")
+
+    def remove_item(self, item):
+        item = item.lower()
+        try:
+            if item.lower() in self.map_items:
+                # If item exists in the list, check to see if there is enough qty to complete transaction
+                i = self.map_items[item.lower()]["item"]
+                qty = self.map_items[item.lower()]["qty"]
+
+                del self.map_items[item]
+
+                return [i, qty]
+            raise ValueError(f"Item {item} does not exist on current map tile")
+        except:
+            raise ValueError("Item name does not exist in MapTile")
+
+
+    def check_item(self, item):
+        if item.lower() in self.map_items:
+            return True
+        else:
+            return False
+
+
+    def has_inventory(self):
+        return True
+
+
+    def tile_items(self):
+        return self.map_items
+
 
 
 class StartTile(MapTile):
